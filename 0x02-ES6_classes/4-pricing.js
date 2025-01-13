6b5cc0f0df27e38420212d54eb5faeb9eadd994a
Raniah06 <raniafadil06@gmail.com>
2025-01-13 20:26:20 +0200

    m

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,8 +2,8 @@ import Currency from './3-currency.js';
 
 class Pricing {
   constructor(amount, currency) {
-    this._amount = this._verifyNumber(amount, 'amount');
-    this._currency = this._verifyCurrency(currency, 'currency');
+    this._amount = amount;
+    this._currency = currency;
   }
 
   get amount() {
@@ -11,7 +11,7 @@ class Pricing {
   }
 
   set amount(newAmount) {
-    this._amount = this._verifyNumber(newAmount, 'amount');
+    this._amount = newAmount;
   }
 
   get currency() {
@@ -19,7 +19,7 @@ class Pricing {
   }
 
   set currency(newCurrency) {
-    this._currency = this._verifyCurrency(newCurrency, 'currency');
+    this._currency = newCurrency;
   }
 
   displayFullPrice() {
@@ -27,27 +27,7 @@ class Pricing {
   }
 
   static convertPrice(amount, conversionRate) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('Amount must be a number');
-    }
-    if (typeof conversionRate !== 'number') {
-      throw new TypeError('Conversion rate must be a number');
-    }
     return amount * conversionRate;
-  }
-
-  _verifyNumber(value, attributeName) {
-    if (typeof value !== 'number') {
-      throw new TypeError(`${attributeName} must be a number`);
-    }
-    return value;
-  }
-
-  _verifyCurrency(value, attributeName) {
-    if (!(value instanceof Currency)) {
-      throw new TypeError(`${attributeName} must be a Currency`);
-    }
-    return value;
   }
 }
 
