752308ba61e34ac295311705581c93850aafffb9
Raniah06 <raniafadil06@gmail.com>
2025-01-13 15:05:20 +0200

    m

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,35 @@
+class Airport {
+  constructor(name, code) {
+    this._name = this._verifyString(name, 'name');
+    this._code = this._verifyString(code, 'code');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = this._verifyString(newName, 'name');
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = this._verifyString(newCode, 'code');
+  }
+
+  toString() {
+    return `[${this._code}]`;
+  }
+
+    _verifyString(value, attributeName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attributeName} must be a string`);
+    }
+    return value;
+  }
+}
+
+export default Airport;
