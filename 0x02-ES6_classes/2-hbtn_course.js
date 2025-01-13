1db8f978df7e05d9f045b87f4bbe02ce853557cc
Raniah06 <raniafadil06@gmail.com>
2025-01-13 19:50:15 +0200

    m

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,17 +1,29 @@
 export default class ALXCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
+    ALXCourse._verifyString(name);
+    ALXCourse._verifyNumber(length);
+    ALXCourse._verifyArray(students);
     this._name = name;
     this._length = length;
     this._students = students;
+  }
+
+  static _verifyString(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  static _verifyNumber(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  static _verifyArray(value) {
+    if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
   }
 
   get name() {
@@ -19,9 +31,7 @@ export default class ALXCourse {
   }
 
   set name(newName) {
-    if (typeof newName !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
+    ALXCourse._verifyString(newName);
     this._name = newName;
   }
 
@@ -30,9 +40,7 @@ export default class ALXCourse {
   }
 
   set length(newLength) {
-    if (typeof newLength !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
+    ALXCourse._verifyNumber(newLength);
     this._length = newLength;
   }
 
@@ -41,9 +49,7 @@ export default class ALXCourse {
   }
 
   set students(newStudents) {
-    if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
+    ALXCourse._verifyArray(newStudents);
     this._students = newStudents;
   }
 }
