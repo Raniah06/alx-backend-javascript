4b4a419ca1e9548475dda92dd9bd17b7a9727ee2
Raniah06 <raniafadil06@gmail.com>
2025-01-13 15:24:49 +0200

    m

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,9 +1,9 @@
 import Building from './5-building.js';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    super(sqft); // Call the parent class constructor
-    this._floors = this._verifyNumber(floors, 'floors');
+    super(sqft);
+    this._floors = floors;
   }
 
   get floors() {
@@ -13,12 +13,4 @@ class SkyHighBuilding extends Building {
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
   }
-    _verifyNumber(value, attributeName) {
-    if (typeof value !== 'number') {
-      throw new TypeError(`${attributeName} must be a number`);
-    }
-    return value;
-  }
 }
-
-export default SkyHighBuilding;
