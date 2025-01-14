5ffcc9ce610080847030dc9be67960fa27b0a58d
Raniah06 <raniafadil06@gmail.com>
2025-01-14 21:44:32 +0200

    Your commit message

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,15 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') return '';
+  if (typeof startString !== 'string' || startString.trim().length === 0) {
+    return '';
+  }
+
   const result = [];
+
   for (const value of set) {
-    if (value.startsWith(startString)) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       result.push(value.slice(startString.length));
     }
   }
+
   return result.join('-');
 }
