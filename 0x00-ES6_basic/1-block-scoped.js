15c93ca8fa0b65625fd33028ae2d8a30c3fc652b
Raniah06 <raniafadil06@gmail.com>
2025-01-14 17:41:41 +0200

    m

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,5 +1,17 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
+export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
+}  let task = false;
   let task2 = true;
 
   if (trueOrFalse) {
