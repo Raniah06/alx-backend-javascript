079bbd4e9b9a7c90fe196e8ea8c764fa126c7328
Raniah06 <raniafadil06@gmail.com>
2025-01-14 18:21:57 +0200

    m

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,7 @@
-import createEmployeesObject from './11-createEmployeesObject';
-import createReportObject from './12-createReportObject';
-import createIteratorObject from './100-createIteratorObject';
-
 export default function iterateThroughObject(reportWithIterator) {
-  const employees = [];
+  let employeeNames = [];
   for (const employee of reportWithIterator) {
-    employees.push(employee);
+    employeeNames.push(employee);
   }
-  return employees.join(' | ');
+  return employeeNames.join(' | ');
 }
