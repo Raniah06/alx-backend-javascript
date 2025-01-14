fd13117e7ca68475f043c595bde17508a6003fae
Raniah06 <raniafadil06@gmail.com>
2025-01-14 16:24:08 +0200

    m

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,32 +1,11 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
 export default class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
-  }
-
-  get brand() {
-    return this[_brand];
-  }
-
-  get motor() {
-    return this[_motor];
-  }
-
-  get color() {
-    return this[_color];
-  }
-
-  static get [Symbol.species]() {
-    return this;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
-    const Species = this.constructor[Symbol.species];
-    return new Species(this.brand, this.motor, this.color);
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
