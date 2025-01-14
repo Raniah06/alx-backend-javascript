ff34d6372fc533814f04d23abc6a91eadab0dbab
Raniah06 <raniafadil06@gmail.com>
2025-01-14 20:27:18 +0200

    m

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -10,7 +10,10 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   return Promise.allSettled(promises).then((results) =>
     results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : String(result.reason),
+      value: result.status === 'fulfilled'
+        ? result.value
+        : String(result.reason),
     }))
   );
 }
+
