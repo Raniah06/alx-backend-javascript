ccdf005faa362952c2c5cd9a879bd2939ef32039
Raniah06 <raniafadil06@gmail.com>
2025-01-14 21:07:23 +0200

    m

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,10 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ];
-
-  return Promise.allSettled(promises).then((results) => 
-    return results.map((result) => ({ // Concise arrow function here
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : String(result.reason),
-    }));
-  );
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
