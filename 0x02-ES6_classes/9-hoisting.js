bbfced7b2b7750474695a6518aae59d4462b943b
Raniah06 <raniafadil06@gmail.com>
2025-01-14 11:06:21 +0200

    m

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -13,9 +13,6 @@ export class ALXClass {
   }
 }
 
-const class2019 = new ALXClass(2019, 'San Francisco');
-const class2020 = new ALXClass(2020, 'San Francisco');
-
 export class StudentALX {
   constructor(firstName, lastName, alxClass) {
     this._firstName = firstName;
@@ -27,14 +24,17 @@ export class StudentALX {
     return `${this._firstName} ${this._lastName}`;
   }
 
-    get alxClass() {
-        return this._alxClass
-    }
+  get alxClass() {
+    return this._alxClass;
+  }
 
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._alxClass.year} - ${this._alxClass.location}`;
   }
 }
+
+const class2019 = new ALXClass(2019, 'San Francisco');
+const class2020 = new ALXClass(2020, 'San Francisco');
 
 const student1 = new StudentALX('Guillaume', 'Salva', class2020);
 const student2 = new StudentALX('John', 'Doe', class2020);
