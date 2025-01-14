15437777e34242da1d95a90f17ece5e7619fd51e
Raniah06 <raniafadil06@gmail.com>
2025-01-14 10:32:28 +0200

    m

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,14 +3,14 @@ class Building {
     if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    this._sqft = this._verifyNumber(sqft, 'sqft');
+    this._sqft = Building._verifyNumber(sqft, 'sqft'); // Use as static method
   }
 
   get sqft() {
     return this._sqft;
   }
 
-    _verifyNumber(value, attributeName) {
+  static _verifyNumber(value, attributeName) { // Make it a static method
     if (typeof value !== 'number') {
       throw new TypeError(`${attributeName} must be a number`);
     }
