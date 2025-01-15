a7f83e60e1fde02df885ab24ae302b98606629e4
Raniah06 <raniafadil06@gmail.com>
2025-01-15 12:22:27 +0200

    m

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,14 @@
-// 4-http.js
 const http = require('http');
 
+const host = '127.0.0.1';
+const port = 1245;
+
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello ALX!');
 });
 
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
-});
+app.listen(port, host, () => {});
 
 module.exports = app;
