dcbb586c5b08f6183ad8d724691462483a98662e
Raniah06 <raniafadil06@gmail.com>
2025-01-15 11:14:28 +0200

    m

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,16 +5,16 @@ process.stdin.setEncoding('utf-8');
 let name = '';
 
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    name += chunk;
-  }
+    const chunk = process.stdin.read();
+    if (chunk !== null) {
+        name += chunk;
+    }
 });
 
 process.stdin.on('end', () => {
-  name = name.trim();
-  if (name) {
-    console.log(`Your name is: ${name}`);
-  }
-  console.log('This important software is now closing');
+    name = name.trim();
+    if (name) {
+        console.log('Your name is: ' + name); // Explicit string concatenation
+    }
+    console.log('This important software is now closing');
 });
