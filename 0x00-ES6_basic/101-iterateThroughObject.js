071e8a1718717538b8f707d52bfa201ce22686f2
Raniah06 <raniafadil06@gmail.com>
2025-01-15 17:57:54 +0200

    m

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,14 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let result = "";
-  let first = true; // Flag to handle the first element
-
-  for (const item of reportWithIterator) {
-    if (!first) {
-      result += " | ";
-    }
-    result += item;
-    first = false;
-  }
-
-  return result;
+  return [...reportWithIterator].join(' | ');
 }
