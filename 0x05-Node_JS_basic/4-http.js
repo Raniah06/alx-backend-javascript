8cd7aabea899339ae4fe861a1022367a849b3ec8
Raniah06 <raniafadil06@gmail.com>
2025-01-15 15:57:55 +0200

    m

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,9 +2,11 @@ const http = require('http');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello ALX!\n');
+  res.end('Hello ALX!');
 });
 
-app.listen(1245);
+app.listen(1245, () => {
+  console.log('Server listening on port 1245');
+});
 
 module.exports = app;
