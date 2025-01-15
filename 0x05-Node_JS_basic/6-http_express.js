cdaa26cdf7c1cc558b4a8b1a98a2728ed6bfa605
Raniah06 <raniafadil06@gmail.com>
2025-01-15 15:39:40 +0200

    m

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -21,11 +21,11 @@ app.get('/students', (req, res) => {
       return;
     }
 
-    const students = data.split('\n').filter(line => line.trim() !== '').map(line => line.split(','));
+    const students = data.split('\n').filter((line) => line.trim() !== '').map((line) => line.split(','));
     const studentCount = students.length;
 
-    const csStudents = students.filter(student => student[1] === 'CS').map(student => student[0]);
-    const sweStudents = students.filter(student => student[1] === 'SWE').map(student => student[0]);
+    const csStudents = students.filter((student) => student[1] === 'CS').map((student) => student[0]);
+    const sweStudents = students.filter((student) => student[1] === 'SWE').map((student) => student[0]);
 
     res.status(200).send(`This is the list of our students\nNumber of students: ${studentCount}\nNumber of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\nNumber of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
   });
